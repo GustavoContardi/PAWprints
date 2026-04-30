4fd1af171671ba26ca0714d9cecde9e5a46d8668
MattZander24 <mattzanderm24mail.com>
2026-04-30 13:27:41 -0300

    About Fix

--- src/Views/about.php
+++ src/Views/about.php
@@ -2,12 +2,12 @@
 <h2>Sobre Nosotros</h2>
 <section>
     <h3>Nuestra Historia</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur explicabo quia nesciunt quas dolores delectus cumque? Deserunt similique impedit accusamus hic, totam quisquam nobis porro saepe. Ipsa aspernatur possimus esse!</p>
-    <img src="../img/About_Thumbnail.png" alt="Foto de la Libreria PAWprints">
+    <p>PAWprints nació de la pasión por la lectura y el deseo de crear un espacio donde las historias dejen una marca duradera en los lectores. Lo que comenzó como un proyecto enfocado en la curaduría de títulos indispensables, se transformó en una librería digital dedicada a conectar a las personas con libros que inspiran, educan y entretienen. Con una identidad visual vibrante y moderna, hemos construido un catálogo que refleja la diversidad de voces y géneros que definen el mundo literario actual.</p>
+    <img src="/assets/img/About_Thumbnail.png" alt="Foto de la Libreria PAWprints">
 </section>
 
 <section>
     <h3>Nuestra Misión</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur explicabo quia nesciunt quas dolores delectus cumque? Deserunt similique impedit accusamus hic, totam quisquam nobis porro saepe. Ipsa aspernatur possimus esse!</p>
-    <img src="../img/logo_PAWprints2.svg" alt="Logotipo de la Libreria PAWprints">
+    <p>En PAWprints, nuestra misión es facilitar el acceso a la cultura y el conocimiento a través de una experiencia de compra intuitiva y personalizada. Nos esforzamos por ser el puente entre los autores y sus lectores, garantizando que cada libro en nuestras estanterías virtuales sea una invitación a descubrir nuevas perspectivas. Buscamos que cada interacción con nuestra librería deje una huella positiva, fomentando el hábito de la lectura en nuestra comunidad.</p>
+    <img src="/assets/img/logo_PAWprints2.svg" alt="Logotipo de la Libreria PAWprints">
 </section>
