50de8ce60951933a4f17732dc0ce30fcd341e6ce
Valentin190306 <valentin.joel.rm@gmail.com>
2026-04-30 23:48:16 -0300

    feat: update contact form placeholders and add subject field

--- src/Views/contact.php
+++ src/Views/contact.php
@@ -6,16 +6,19 @@
         <legend>Formulario de Contacto</legend>
 
         <label for="nombre">Nombre</label>
-        <input type="text" name="nombre" id="nombre" placeholder="Valor" required>
+        <input type="text" name="nombre" id="nombre" placeholder="Tu nombre" required>
 
         <label for="apellido">Apellido</label>
-        <input type="text" name="apellido" id="apellido" placeholder="Valor" required>
+        <input type="text" name="apellido" id="apellido" placeholder="Tu apellido" required>
 
         <label for="email">Email</label>
-        <input type="email" name="email" id="email" placeholder="Valor" required>
+        <input type="email" name="email" id="email" placeholder="un-email@ejemplo.com" required>
+
+        <label for="asunto">Asunto</label>
+        <input type="text" name="asunto" id="asunto" placeholder="Asunto del mensaje" required>
             
         <label for="mensaje">Mensaje</label>
-        <textarea name="mensaje" id="mensaje" placeholder="Valor" required></textarea>
+        <textarea name="mensaje" id="mensaje" placeholder="Escribe aqui tu mensaje" required></textarea>
         <button type="submit">Enviar</button>
     </fieldset>
 </form>
